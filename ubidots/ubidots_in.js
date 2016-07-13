0d8af84d1bd925b7cff91e22081c53bbd092ac03
Gustavo Andrés Angulo <woakas@gmail.com>
2016-07-13 08:53:40 -0500

    Indent Javascript and removing some lines that not used

--- ubidots/ubidots_in.js
+++ ubidots/ubidots_in.js
@@ -1,77 +1,73 @@
 module.exports = function (RED) {
-    var mqtt = require('mqtt');
-	function getClient(x, n){
-		if(x.client !== null && x.client !== undefined){
-			x.client.end(true, function(){});
-		}
-		try {
-            var client = mqtt.connect('mqtt://things.ubidots.com', {username: n.token, password: ""});
-			x.client = client;
-            client.on("error", function () {
-				client.end(true, function(){});
-                x.status({fill: "red", shape: "ring", text: "disconnected"});
-            });
-			client.on('close', function(){
-				client.end(true, function(){});
-			});
-            client.on("reconnect", function () {
-                x.status({fill: "green", shape: "dot", text: "connected"});
-                var topic = "/v1.6/devices/" + n.label_data_source + "/" + n.label_variable;
-                var options = {};
-                options[topic] = 1;
-                client.subscribe(options, function (err, granted) {
-                    try {
-                        client.on('message', function (topic, message, packet) {
-                            x.emit("input", {payload: JSON.parse(message.toString())});
-                        });
-                    } catch (e) {
-                        x.status({fill: "red", shape: "ring", text: "disconnected"});
-                    }
-                });
-            });
-            client.on("connect", function () {
-                x.status({fill: "green", shape: "dot", text: "connected"});
-                var topic = "/v1.6/devices/" + n.label_data_source + "/" + n.label_variable;
-                var options = {};
-                options[topic] = 1;
-                client.subscribe(options, function (err, granted) {
-                    try {
-                        client.on('message', function (topic, message, packet) {
-                            x.emit("input", {payload: JSON.parse(message.toString())});
-                        });
-                    } catch (e) {
-                        x.status({fill: "red", shape: "ring", text: "disconnected"});
-                    }
-                });
-            });
-        } catch (e) {
-            x.status({fill: "red", shape: "ring", text: "disconnected"});
-        }	
-	}
-    function UbidotsNode(n) {
-        RED.nodes.createNode(this, n);
-        var x = this;
-		getClient(x, n);
-        x.status({fill: "red", shape: "ring", text: "disconnected"});
-        
-        this.on("error", function () {
-			if(x.client !== null && x.client !== undefined){
-				x.client.end(true, function(){});
-			}
-            x.status({fill: "red", shape: "ring", text: "disconnected"});
-        });
-		this.on("close", function(){
-			if(x.client !== null && x.client !== undefined){
-				x.client.end(true, function(){});
-			}
-		});
-        this.on("input", function (msg) {
-            try {
-                this.send(msg);
-            } catch (err) {
-                this.error(err, msg);
-            }
-        });
+  var mqtt = require('mqtt');
+  function getClient(that, n){
+    if(that.client !== null && that.client !== undefined){
+      that.client.end(true, function(){});
     }
-    RED.nodes.registerType("ubidots_in", UbidotsNode);
+    var client = mqtt.connect('mqtt://things.ubidots.com', {username: n.token, password: ""});
+    that.client = client;
+    client.on("error", function () {
+      client.end(true, function(){});
+      that.status({fill: "red", shape: "ring", text: "disconnected"});
+    });
+    client.on('close', function(){
+      client.end(true, function(){});
+    });
+    client.on("reconnect", function () {
+      that.status({fill: "green", shape: "dot", text: "connected"});
+      var topic = "/v1.6/devices/" + n.label_data_source + "/" + n.label_variable;
+      var options = {};
+      options[topic] = 1;
+      client.subscribe(options, function (err, granted) {
+        try {
+          client.on('message', function (topic, message, packet) {
+            that.emit("input", {payload: JSON.parse(message.toString())});
+          });
+        } catch (e) {
+          that.status({fill: "red", shape: "ring", text: "disconnected"});
+        }
+      });
+    });
+    client.on("connect", function () {
+      that.status({fill: "green", shape: "dot", text: "connected"});
+      var topic = "/v1.6/devices/" + n.label_data_source + "/" + n.label_variable;
+      var options = {};
+      options[topic] = 1;
+      client.subscribe(options, function (err, granted) {
+        try {
+          client.on('message', function (topic, message, packet) {
+            that.emit("input", {payload: JSON.parse(message.toString())});
+          });
+        } catch (e) {
+          that.status({fill: "red", shape: "ring", text: "disconnected"});
+        }
+      });
+    });
+  }
+  function UbidotsNode(n) {
+    RED.nodes.createNode(this, n);
+    var that = this;
+    getClient(that, n);
+    that.status({fill: "red", shape: "ring", text: "disconnected"});
+    
+    this.on("error", function () {
+      if(that.client !== null && that.client !== undefined){
+	that.client.end(true, function(){});
+      }
+      that.status({fill: "red", shape: "ring", text: "disconnected"});
+    });
+    this.on("close", function(){
+      if(that.client !== null && that.client !== undefined){
+	that.client.end(true, function(){});
+      }
+    });
+    this.on("input", function (msg) {
+      try {
+        this.send(msg);
+      } catch (err) {
+        this.error(err, msg);
+      }
+    });
+  }
+  RED.nodes.registerType("ubidots_in", UbidotsNode);
 };
