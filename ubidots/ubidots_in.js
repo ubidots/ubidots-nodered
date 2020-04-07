8e63d32abd0faa22c04e916f68931d7d0a412085
Benjamin Heinke <benjamin@ubidots.com>
2020-04-07 15:30:43 -0500

    additional fixes and UI optimizations

--- ubidots/ubidots_in.js
+++ ubidots/ubidots_in.js
@@ -1,7 +1,7 @@
 module.exports = function (RED) {
-  var mqtt = require("mqtt");
-  var fs = require("fs");
-  var path = require("path");
+  var mqtt = require('mqtt');
+  var fs = require('fs');
+  var path = require('path');
 
   function getClient(
     self,
@@ -13,78 +13,78 @@ module.exports = function (RED) {
     token,
     useCustomTopics
   ) {
-    self.status({ fill: "green", shape: "ring", text: "ubidots.connecting" });
-    var URL_PREFIX = "mqtt://";
+    self.status({ fill: 'green', shape: 'ring', text: 'ubidots.connecting' });
+    var URL_PREFIX = 'mqtt://';
     var port = 1883;
     var portTLS = 8883;
     var certificate = fs.readFileSync(
-      path.join(__dirname, "../keys/certificate.pem"),
-      "utf8",
+      path.join(__dirname, '../keys/certificate.pem'),
+      'utf8',
       function () {}
     );
 
     var client = mqtt.connect(URL_PREFIX + endpointUrl, {
       username: token,
-      password: "",
+      password: '',
       port: useTLS ? portTLS : port,
       cert: useTLS ? certificate : undefined,
-      protocol: useTLS ? "mqtts" : "mqtt"
+      protocol: useTLS ? 'mqtts' : 'mqtt'
     });
 
-    client.on("error", function () {
+    client.on('error', function () {
       client.end(true, function () {});
       self.status({
-        fill: "red",
-        shape: "ring",
-        text: "ubidots.error_connecting"
+        fill: 'red',
+        shape: 'ring',
+        text: 'ubidots.error_connecting'
       });
     });
 
-    client.on("close", function () {
+    client.on('close', function () {
       client.end(true, function () {});
     });
 
-    client.on("reconnect", function () {
-      console.log("Client reconnecting");
+    client.on('reconnect', function () {
+      console.log('Client reconnecting');
       var options = { qos: 1 };
       self.status({
-        fill: "yellow",
-        shape: "ring",
-        text: "ubidots.connecting"
+        fill: 'yellow',
+        shape: 'ring',
+        text: 'ubidots.connecting'
       });
 
       client.subscribe(topics, options, function () {
         try {
-          client.on("message", function (topic, message, packet) {
-            self.emit("input", { payload: JSON.parse(message.toString()) });
+          client.on('message', function (topic, message, packet) {
+            self.emit('input', { payload: JSON.parse(message.toString()) });
           });
         } catch (e) {
           self.status({
-            fill: "red",
-            shape: "ring",
-            text: "ubidots.error_connecting"
+            fill: 'red',
+            shape: 'ring',
+            text: 'ubidots.error_connecting'
           });
         }
       });
     });
 
-    client.on("connect", function () {
-      console.log("Client connected");
+    client.on('connect', function () {
+      console.log('Client connected');
       var options = { qos: 1 };
 
-      self.status({ fill: "green", shape: "dot", text: "ubidots.connected" });
+      self.status({ fill: 'green', shape: 'dot', text: 'ubidots.connected' });
       client.subscribe(topics, options, function (err, granted) {
         try {
-          client.on("message", function (topic, message, packet) {
+          client.on('message', function (topic, message, packet) {
             let finalObject = defineOutputObject(topic, message);
-            self.emit("input", { payload: finalObject });
+            self.emit('input', { payload: finalObject });
           });
         } catch (e) {
-          console.log("Error when trying to emit: ", e);
+          console.log('Error when trying to emit: ', e);
           self.status({
-            fill: "red",
-            shape: "ring",
-            text: "ubidots.error_connecting"
+            fill: 'red',
+            shape: 'ring',
+            text: 'ubidots.error_connecting'
           });
         }
       });
@@ -95,37 +95,39 @@ module.exports = function (RED) {
       let variable = topic;
       if (useCustomTopics) {
         variable = topic.substring(14);
-        if (topic.substring(topic.length - 3) === "/lv") {
-          finalObject[variable] = { value: JSON.parse(message.toString()) };
-        } else {
-          finalObject[variable] = JSON.parse(message.toString());
-        }
+        finalObject = parseOutputObject(topic, variable, message);
       } else {
-        if (topic.substring(topic.length - 3) === "/lv") {
+        if (topic.endsWith('/lv')) {
           variable = topic.slice(0, topic.length - 3);
         }
-        let array = variable.split("/");
-        variable = array[array.length - 1];
-        if (topic.substring(topic.length - 3) === "/lv") {
-          finalObject[variable] = { value: JSON.parse(message.toString()) };
-        } else {
-          finalObject[variable] = JSON.parse(message.toString());
-        }
+        let topicElements = variable.split('/');
+        variable = topicElements[topicElements.length - 1];
+        finalObject = parseOutputObject(topic, variable, message);
       }
       return finalObject;
     }
+  }
+
+  function parseOutputObject(topic, variable, message) {
+    let finalObject = {};
+    if (topic.endsWith('/lv')) {
+      finalObject[variable] = { value: JSON.parse(message.toString()) };
+    } else {
+      finalObject[variable] = JSON.parse(message.toString());
+    }
+    return finalObject;
   }
 
   function UbidotsNode(config) {
     RED.nodes.createNode(this, config);
     var self = this;
     var ENDPOINTS_URLS = {
-      business: "industrial.api.ubidots.com",
-      educational: "things.ubidots.com"
+      business: 'industrial.api.ubidots.com',
+      educational: 'things.ubidots.com'
     };
     var useTLS = config.tls_checkbox_in;
     var labelDevice = config.device_label;
-    var labelVariable = config["label_variable_1"];
+    var labelVariable = config['label_variable_1'];
     var endpointUrl = ENDPOINTS_URLS[config.tier] || ENDPOINTS_URLS.business;
     var token = config.token;
     var useCustomTopics = config.custom_topic_checkbox;
@@ -144,29 +146,29 @@ module.exports = function (RED) {
       useCustomTopics
     );
 
-    this.on("error", function (msg) {
-      console.log("Client: Inside error function", msg);
+    this.on('error', function (msg) {
+      console.log('Client: Inside error function', msg);
       if (self.client !== null && self.client !== undefined) {
         self.client.end(true, function () {});
       }
       self.status({
-        fill: "red",
-        shape: "ring",
-        text: "ubidots.error_connecting"
+        fill: 'red',
+        shape: 'ring',
+        text: 'ubidots.error_connecting'
       });
     });
 
-    this.on("close", function () {
+    this.on('close', function () {
       if (self.client !== null && self.client !== undefined) {
         self.client.end(true, function () {});
       }
     });
 
-    this.on("input", function (msg, send, done) {
+    this.on('input', function (msg, send, done) {
       try {
         send(msg);
       } catch (err) {
-        console.log("Error in client when sending data to debug node,", err);
+        console.log('Error in client when sending data to debug node,', err);
         this.error(err, msg);
       }
       if (done) {
@@ -175,37 +177,37 @@ module.exports = function (RED) {
     });
   }
 
-  RED.nodes.registerType("ubidots_in", UbidotsNode);
+  RED.nodes.registerType('ubidots_in', UbidotsNode);
 };
 
 function getSubscribePaths(config) {
   var paths = [];
-  var labelString = "label_variable_";
-  var completeLabelString = "";
-  var checkboxString = "checkbox_variable_";
-  var checkboxString2 = "_last_value";
-  var completeCheckboxString = "";
+  var labelString = 'label_variable_';
+  var completeLabelString = '';
+  var checkboxString = 'checkbox_variable_';
+  var checkboxString2 = '_last_value';
+  var completeCheckboxString = '';
   //use customtopics
   if (config.custom_topic_checkbox) {
     for (var i = 1; i < 11; i++) {
       completeLabelString = labelString + i.toString();
-      if (!(config[completeLabelString] === "")) {
-        paths.push("/v1.6/devices/" + config[completeLabelString]);
+      if (!(config[completeLabelString] === '')) {
+        paths.push('/v1.6/devices/' + config[completeLabelString]);
       }
     }
   } else {
     for (var i = 1; i < 11; i++) {
       completeLabelString = labelString + i.toString();
       completeCheckboxString = checkboxString + i.toString() + checkboxString2;
-      if (!(config[completeLabelString] === "")) {
+      if (!(config[completeLabelString] === '')) {
         //if last value checkbox is checked
         var devicePath =
-          "/v1.6/devices/" +
+          '/v1.6/devices/' +
           config.device_label +
-          "/" +
+          '/' +
           config[completeLabelString];
         if (config[completeCheckboxString]) {
-          devicePath += "/lv";
+          devicePath += '/lv';
         }
         paths.push(devicePath);
       }
--- ubidots/ubidots_out.js
+++ ubidots/ubidots_out.js
@@ -1,68 +1,67 @@
-module.exports = function(RED) {
-  var mqtt = require("mqtt");
-  var fs = require("fs");
-  var path = require("path");
+module.exports = function (RED) {
+  var mqtt = require('mqtt');
+  var fs = require('fs');
+  var path = require('path');
 
   function UbidotsNode(config) {
     RED.nodes.createNode(this, config);
     var self = this;
     var ENDPOINT_URLS = {
-      business: "industrial.api.ubidots.com",
-      educational: "things.ubidots.com"
+      business: 'industrial.api.ubidots.com',
+      educational: 'things.ubidots.com'
     };
     var useTLS = config.tls_checkbox;
-    var URL_PREFIX = "mqtt://";
+    var URL_PREFIX = 'mqtt://';
 
     var port = 1883;
     var portTLS = 8883;
     var certificate = fs.readFileSync(
-      path.join(__dirname, "../keys/certificate.pem"),
-      "utf8",
-      function() {}
+      path.join(__dirname, '../keys/certificate.pem'),
+      'utf8',
+      function () {}
     );
 
     var endpointUrl = ENDPOINT_URLS[config.tier] || ENDPOINT_URLS.business;
     var token = config.token;
     var client = mqtt.connect(URL_PREFIX + endpointUrl, {
       username: token,
-      password: "",
+      password: '',
       port: useTLS ? portTLS : port,
       cert: useTLS ? certificate : undefined,
-      protocol: useTLS ? "mqtts" : "mqtt",
-   
+      protocol: useTLS ? 'mqtts' : 'mqtt'
     });
 
-    client.on("reconnect", function() {
+    client.on('reconnect', function () {
       self.status({
-        fill: "yellow",
-        shape: "ring",
-        text: "ubidots.reconnecting"
+        fill: 'yellow',
+        shape: 'ring',
+        text: 'ubidots.reconnecting'
       });
     });
 
-    client.on("connect", function(connack) {
-      console.log("Publisher connected");
-      self.status({ fill: "green", shape: "dot", text: "Connected" });
+    client.on('connect', function (connack) {
+      console.log('Publisher connected');
+      self.status({ fill: 'green', shape: 'dot', text: 'Connected' });
     });
-    client.on("disconnect", function(packet) {
-      console.log("Publisher disconnected");
-      self.status({ fill: "red", shape: "ring", text: "Disconnected" });
+    client.on('disconnect', function (packet) {
+      console.log('Publisher disconnected');
+      self.status({ fill: 'red', shape: 'ring', text: 'Disconnected' });
     });
 
-    client.on("error", function(msg) {
-      console.warn("Publisher: Inside error function, msg: ", msg);
+    client.on('error', function (msg) {
+      console.warn('Publisher: Inside error function, msg: ', msg);
       self.status({
-        fill: "red",
-        shape: "ring",
-        text: "ubidots.error_connecting"
+        fill: 'red',
+        shape: 'ring',
+        text: 'ubidots.error_connecting'
       });
     });
 
-    self.on("input", function(msg, send, done) {
+    self.on('input', function (msg, send, done) {
       //In case the msg contains a property named'ubidotsDeviceLabel'
       //it will be taken as device_label, otherwise it takes it from the device_label field
       var device_label = msg.payload.ubidotsDeviceLabel || config.device_label;
-      if (device_label === undefined || device_label === "") {
+      if (device_label === undefined || device_label === '') {
         console.error(
           "Device_Label is not defined. The device_label field is probably empty or you didn't include the key 'ubidotsDeviceLabel' in your JSON."
         );
@@ -71,24 +70,24 @@ module.exports = function(RED) {
           delete msg.payload.ubidotsDeviceLabel;
         }
         var values =
-          typeof msg.payload !== "object" || msg.payload === null
+          typeof msg.payload !== 'object' || msg.payload === null
             ? {}
             : msg.payload;
 
-        if (typeof values === "object") {
+        if (typeof values === 'object') {
           values = JSON.stringify(values);
         }
         try {
           client.publish(
-            "/v1.6/devices/" + device_label,
+            '/v1.6/devices/' + device_label,
             values,
             { qos: 1, retain: false },
-            function() {
-              console.log("Published successfully,");
+            function () {
+              console.log('Published successfully,');
             }
           );
         } catch (e) {
-          console.log("Published failed: ", e);
+          console.log('Published failed: ', e);
         }
       }
       if (done) {
@@ -97,5 +96,5 @@ module.exports = function(RED) {
     });
   }
 
-  RED.nodes.registerType("ubidots_out", UbidotsNode);
+  RED.nodes.registerType('ubidots_out', UbidotsNode);
 };
